0d093fa3b719409381b58150eabc2250e632af0a
Artem <milkartem2@mail.ru>
2023-04-20 16:28:03 +0300

    done hw1

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Homework1 {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        // Err1
+//        int num1 = getNumber();
+//        System.out.println(num1);
+
+        //Err2
+//        int[] array1 = {1, 2, 3, 4};
+//        printArray(array1);
+
+        //Err3
+//        int[] array2 = null;
+//        int[] array3 = addArray(array2);
+//        System.out.println(Arrays.toString(array3));
+        int[] a = {3, 2, 1, 4}, b = {2, 5, 11};
+        int[] res = sumElementsArray(a, b);
+
+        if (res == null) throw new RuntimeException("Null in res");
+
+        for (int element : res) {
+            System.out.println(element);
+        }
+
+    }
+
+    // InputMismatchException
+    static int getNumber() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input number");
+        return scanner.nextInt();
+    }
+
+    // ArrayIndexOutOfBoundsException
+    static void printArray(int[] arr) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    // NullPointerException
+    static int[] addArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * 2;
+        }
+
+        return arr;
+    }
+
+    static int[] sumElementsArray(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            System.out.println("Diff arrays length");
+            return null;
+        }
+
+        int[] res = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            res[i] = arr1[i] + arr2[i];
+        }
+        return res;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = null;
+        System.out.println(showArray(arr1, 2));
+        System.out.println(findElementIndex(arr1, 3, 3));
+
+        int[][] arr2 = {
+                {2, 3, 4},
+                {3, 4, 5}
+        };
+        System.out.println(arr2.length + arr2[0].length);
+    }
+
+    public static int showArray(int[] arr, int limit) {
+        if (arr == null || arr.length > limit) {
+            return -1;
+        }
+        return arr.length;
+    }
+
+    public static int findElementIndex(int[] arr, int element, int minLength) {
+        if (arr == null) {
+            return -3;
+        }
+        if (arr.length < minLength) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                return i;
+            }
+        }
+        return -2;
+    }
+
+    public static int sumDoubleArray(int[][] array) {
+        int sum = 0;
+
+        if (array.length != array[0].length) {
+            throw new RuntimeException("не квадратный массив");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] != 0 || array[i][j] != 1) {
+                    throw new RuntimeException("число не равно 0 или 1");
+                }
+                sum += array[i][j];
+            }
+        }
+
+        return sum;
+    }
 }
+
+
+
