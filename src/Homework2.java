e98f2024ab8d0a15fc3523fb3139aeec931ab086
Artem <milkartem2@mail.ru>
2023-04-24 13:58:49 +0300

    Hw2 done

---
+++ src/Homework2.java
@@ -0,0 +1,60 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) throws Exception {
+
+//        float number = inputFloat();
+//        System.out.println(number);
+//
+//         task2();
+
+         taks3();
+    }
+
+    static float inputFloat() {
+        float num;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Input float: ");
+                num = scanner.nextFloat();
+                break;
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.getClass().getSimpleName());
+                scanner.nextLine();
+            }
+        }
+        return num;
+    }
+
+    static void task2() throws Exception{
+        try {
+            int d = 0;
+            //added init of intArray
+            double[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            double catchedRes1 = intArray[8] / d;
+
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+
+    public static void taks3() throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 , 3, 4};
+            abc[3] = 9;
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
+}
